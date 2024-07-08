27efa2369893962203e90a66a9684dea5cf8b628
Burak Karaman <karaman.buraak@gmail.com>
2024-07-08 10:11:07 +0200

    Update .gitignore, index.js, user.js, and routes files with code formatting and syntax changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,62 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const { CustomError } = require("../errors/customError");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      //   validate: [
+      //     (password) =>
+      //       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}])[A-Za-z\d!-\*?+&%{}]{8,}$/.test(
+      //         password
+      //       ),
+      //     "Password type is incorrect",
+      //   ],
+      set: (password) => {
+        if (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-\*?+&%{}])[A-Za-z\d!-\*?+&%{}]{8,}$/.test(
+            password
+          )
+        ) {
+          return passwordEncrypt(password);
+        } else {
+          throw new CustomError("Password type is incorrect", 400);
+        }
+      },
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+const user = require("../controllers/user");
+const idValidation = require("../middlewares/idValidation");
+
+router.route("/").get(user.list).post(user.create);
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(user.read)
+  .put(user.update)
+  .patch(user.update)
+  .delete(user.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
