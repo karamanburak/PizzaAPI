d6909ddf1148aea834b61ccc6d7f9586de0f1156
Burak Karaman <karaman.buraak@gmail.com>
2024-07-08 10:37:55 +0200

    Update token and user models: add TokenSchema with userId and token fields, and add email validation to UserSchema.

--- src/models/token.js
+++ src/models/token.js
@@ -1,6 +1,30 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const TokenSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    token: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true,
+      unique: true,
+    },
+  },
+  {
+    collection: "tokens",
+    timestamps: false,
+  }
+);
+
+module.exports = mongoose.model("Token", TokenSchema);
