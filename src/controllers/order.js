ba1b71bfeacfca4d58cb249f3b004489263011cc
Burak Karaman <karaman.buraak@gmail.com>
2024-07-08 12:40:07 +0200

    Add CRUD functionality to Order controller and model, and define routes for orders

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -2,3 +2,51 @@
 /* -------------------------------------------------------
                         PIZZA API
 ------------------------------------------------------- */
+const Order = require("../models/order");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Order);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails,
+      data,
+    });
+  },
+  //! CRUD(Create-Read-Update-Delete)
+  create: async (req, res) => {
+    // delete req.body.amount; // amount alanini db ye eklememek icin
+    const data = await Order.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Order.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    // delete req.body.amount; // amount alanini db ye eklememek icin, amoun hesaplamasini getter ile yapariz!!!
+
+    const data = await Order.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    res.status(202).send({
+      error: false,
+      data,
+      newData: await Order.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Order.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+      message: "Pizza not found!",
+    });
+  },
+};
